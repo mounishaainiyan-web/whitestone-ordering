3d3a1e8e8509de1404720a0756a13b7ae90ead6e
Mounishaa <mounishaainiyan@gmail.com>
2026-02-20 13:58:15 +0530

    Added default route

--- client/src/data/menu.js
+++ client/src/data/menu.js
@@ -1,96 +1,131 @@
 const menu = [
+
+  // 🍨 ICE CREAMS
   {
     category: "Ice Creams",
     items: [
-      { name: "Vanilla", price: 50 },
-      { name: "Strawberry", price: 50 },
-      { name: "Mango", price: 60 },
-      { name: "Butterscotch", price: 70 },
-      { name: "Chocolate", price: 70 },
-      { name: "Oreo", price: 70 },
-      { name: "Almond", price: 70 },
-      { name: "Blackcurrant", price: 70 },
-      { name: "Dry Fruit", price: 80 },
-      { name: "Fig", price: 80 }
-    ]
+      { name: "Vanilla", price: { single: 50, double: 80, family: 180 } },
+      { name: "Strawberry", price: { single: 50, double: 80, family: 180 } },
+      { name: "Mango", price: { single: 60, double: 90, family: 220 } },
+      { name: "Butterscotch", price: { single: 70, double: 100, family: 260 } },
+      { name: "Chocolate", price: { single: 70, double: 100, family: 260 } },
+      { name: "Oreo", price: { single: 70, double: 100, family: 260 } },
+      { name: "Almond", price: { single: 70, double: 100, family: 260 } },
+      { name: "Blackcurrant", price: { single: 70, double: 100, family: 260 } },
+      { name: "Dry Fruit", price: { single: 80, double: 120, family: 290 } },
+      { name: "Fig", price: { single: 80, double: 120, family: 290 } },
+    ],
   },
 
+  // 🍟 VEG FRIES
   {
     category: "Veg Fries",
     items: [
       { name: "French Fries", price: 80 },
       { name: "Potato Smiley (8 pcs)", price: 90 },
       { name: "Veg Nuggets (8 pcs)", price: 90 },
-      { name: "Chilli Garlic Potato Pops", price: 100 },
-      { name: "Potato Cheese Shots", price: 100 },
+      { name: "Chilli Garlic Potato Pops (15 pcs)", price: 100 },
+      { name: "Potato Cheese Shots (8 pcs)", price: 100 },
       { name: "Masala French Fries", price: 100 },
-      { name: "Veggie Fingers", price: 110 },
-      { name: "Falafel", price: 110 }
-    ]
+      { name: "Veggie Fingers (6 pcs)", price: 110 },
+      { name: "Falafel (10 pcs)", price: 110 },
+      { name: "Cheesy Fries", price: 150 },
+      { name: "Loaded Fries", price: 170 },
+    ],
   },
 
+  // 🍗 NON-VEG FRIES
   {
     category: "Non Veg Fries",
     items: [
       { name: "Chicken Nuggets (6 pcs)", price: 100 },
       { name: "Chicken Pops (15 pcs)", price: 110 },
       { name: "Chicken Fingers (6 pcs)", price: 110 },
-      { name: "Chicken Hot Wings", price: 120 },
-      { name: "Chicken Cheese Balls", price: 120 }
-    ]
+      { name: "Chicken Hot Wings (4 pcs)", price: 120 },
+      { name: "Chicken Cheese Balls (6 pcs)", price: 120 },
+      { name: "Chicken French Fries", price: 120 },
+      { name: "Chicken Lollipop (4 pcs)", price: 130 },
+      { name: "Chicken Bites (100g)", price: 130 },
+      { name: "Fried Chicken Snackers (5 pcs)", price: 130 },
+      { name: "Garlic Chicken Fingers (6 pcs)", price: 120 },
+      { name: "Fish Fingers (6 pcs)", price: 110 },
+    ],
   },
 
-  {
-    category: "Milkshakes",
-    items: [
-      { name: "Rose Milk", price: 70 },
-      { name: "Vanilla Shake", price: 90 },
-      { name: "Strawberry Shake", price: 90 },
-      { name: "Mango Shake", price: 90 },
-      { name: "Chocolate Shake", price: 100 },
-      { name: "Oreo Shake", price: 120 },
-      { name: "KitKat Shake", price: 140 },
-      { name: "Nutella Shake", price: 160 }
-    ]
-  },
-
+  // 🍕 VEG PIZZA
   {
     category: "Veg Pizza",
     items: [
-      { name: "Classy Margherita (Small)", price: 100 },
-      { name: "Double Cheese Margherita (Small)", price: 140 },
-      { name: "Corn and Cheese (Small)", price: 160 },
-      { name: "Cheesy Paneer (Small)", price: 160 },
-      { name: "Veggie Overloaded (Small)", price: 200 }
-    ]
+      { name: "Classy Margherita", price: { small: 100, medium: 180 } },
+      { name: "Double Cheese Margherita", price: { small: 140, medium: 240 } },
+      { name: "Corn and Cheese", price: { small: 160, medium: 250 } },
+      { name: "Cheesy Paneer", price: { small: 160, medium: 260 } },
+      { name: "Pepper Paneer", price: { small: 180, medium: 280 } },
+      { name: "Corn and Veg", price: { small: 180, medium: 280 } },
+      { name: "Peri Peri Corn", price: { small: 190, medium: 290 } },
+      { name: "Peri Peri Paneer", price: { small: 190, medium: 290 } },
+      { name: "Veg Only", price: { small: 190, medium: 290 } },
+      { name: "Veggie Overloaded", price: { small: 200, medium: 300 } },
+      { name: "Veg Pepper Paneer", price: { small: 220, medium: 320 } },
+      { name: "Mushroom Cheese", price: { small: 220, medium: 320 } },
+      { name: "Veggie Feast", price: { small: 230, medium: 320 } },
+    ],
   },
 
+  // 🍗 NON-VEG PIZZA
   {
     category: "Non Veg Pizza",
     items: [
-      { name: "Chicken Pepperoni (Small)", price: 220 },
-      { name: "Double Chicken (Small)", price: 260 }
-    ]
+      { name: "Chicken Pepperoni", price: { small: 220, medium: 320 } },
+      { name: "Double Chicken", price: { small: 260, medium: 360 } },
+    ],
   },
 
+  // 🍔 VEG BURGER
   {
     category: "Veg Burger",
     items: [
-      { name: "Veg Burger", price: 100 },
-      { name: "Veg Cheese Burger", price: 120 },
-      { name: "Paneer Burger", price: 120 },
-      { name: "Paneer Cheese Burger", price: 140 }
-    ]
+      { name: "Veg", price: 100 },
+      { name: "Veg Cheese", price: 120 },
+      { name: "Veg Double Cheese", price: 130 },
+      { name: "Paneer", price: 120 },
+      { name: "Paneer Cheese", price: 140 },
+      { name: "Spinach Corn", price: 120 },
+      { name: "Veg Volcano Burger", price: 160 },
+      { name: "Paneer Volcano Burger", price: 170 },
+    ],
   },
 
+  // 🍗 NON-VEG BURGER
   {
     category: "Non Veg Burger",
     items: [
-      { name: "Classic Chicken Burger", price: 110 },
-      { name: "Chicken Cheese Burger", price: 140 },
-      { name: "Peri Peri Chicken Burger", price: 150 }
-    ]
-  }
+      { name: "Classic Chicken", price: 110 },
+      { name: "Chicken", price: 120 },
+      { name: "Chicken Cheese", price: 140 },
+      { name: "Chicken Double Cheese", price: 150 },
+      { name: "Peri Peri Chicken Burger", price: 150 },
+      { name: "Tandoori Chicken Burger", price: 130 },
+      { name: "Spanish Chicken Burger", price: 140 },
+      { name: "Chicken Volcano Burger", price: 180 },
+    ],
+  },
+
+  // 🥤 MILKSHAKES
+  {
+    category: "Milkshakes",
+    items: [
+      { name: "Rose Milk", price: { normal: 70, thick: 110 } },
+      { name: "Vanilla", price: { normal: 90, thick: 130 } },
+      { name: "Strawberry", price: { normal: 90, thick: 130 } },
+      { name: "Mango", price: { normal: 90, thick: 130 } },
+      { name: "Chocolate", price: { normal: 100, thick: 140 } },
+      { name: "Oreo", price: { normal: 120, thick: 160 } },
+      { name: "Nutella", price: { normal: 140, thick: 190 } },
+      { name: "Lotus Biscoff", price: { normal: 170, thick: 220 } },
+    ],
+  },
+
 ];
 
 export default menu;
--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ app.use("/api/orders", orderRoutes);
 mongoose.connect("mongodb+srv://admin:Whitestone123@cluster0.6adfpad.mongodb.net/?retryWrites=true&w=majority")
   .then(() => console.log("MongoDB Atlas Connected"))
   .catch(err => console.error(err));
+  app.get("/", (req, res) => {
+  res.send("Whitestone Ordering Server is Running 🚀");
+});
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
