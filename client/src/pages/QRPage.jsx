8b41bd4641cfc5287ab50645a178a1c11d1c4cdd
Mounishaa <mounishaainiyan@gmail.com>
2026-02-25 19:45:32 +0530

    Fixed QR page display

--- client/src/pages/QRPage.jsx
+++ client/src/pages/QRPage.jsx
@@ -1,24 +1,30 @@
 import { QRCodeCanvas } from "qrcode.react";
 
 function QRPage() {
-const url = "https://whitestone-ordering.onrender.com";
+  const url = "https://whitestone-ordering.onrender.com/";
 
   return (
     <div
       style={{
         textAlign: "center",
         padding: "50px",
-        fontFamily: "Arial",
+        fontFamily: "Arial, sans-serif",
       }}
     >
       <h1>Scan To Order</h1>
 
-      <QRCodeCanvas
-        value={url}
-        size={250}
-      />
+      <div style={{ marginTop: "30px" }}>
+        <QRCodeCanvas
+          value={url}
+          size={250}
+          bgColor={"#ffffff"}
+          fgColor={"#000000"}
+          level={"H"}
+          includeMargin={true}
+        />
+      </div>
 
-      <p style={{ marginTop: "20px" }}>
+      <p style={{ marginTop: "20px", fontSize: "16px" }}>
         {url}
       </p>
     </div>
